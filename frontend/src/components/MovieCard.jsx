010f457d8d9380dc0efb67237b8a5a3289847568
Mohammad Anas <mdanascal@gmail.com>
2026-04-28 21:58:02 +0530

    Replace hardcoded movie card with dynamic rendering

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -2,19 +2,41 @@ import React, { useContext } from "react";
 import { movieSearchResult } from "../context/MovieSearchResultContext";
 import { FaStar } from "react-icons/fa";
 const MovieCard = () => {
-  const { movies } = useContext(movieSearchResult);
-  console.log(movies);
+  let { movies } = useContext(movieSearchResult);
 
-  //   if (!movies || movies.length === 0) return null;
+  if (!movies || movies.length === 0) return null;
+  movies = movies.map((movie) => ({
+    ...movie,
+    vote_average: (movie.vote_average / 10) * 5,
+  }));
+
   return (
     <>
-      {/* Temporary div to check fetched movies
-      <div className="absolute top-16 left-0 w-full bg-gray-800 text-white p-4 z-50 flex flex-col gap-2">
+      <div className="bg-gray-300 p-3 flex flex-wrap gap-2 justify-evenly">
         {movies.map((movie) => (
-          <div key={movie.id}>{movie.title}</div>
+          <div
+            className="flex px-5 py-3 w-1/4 h-52 gap-1 border rounded-xl bg-white hover:scale-110 transition-all duration-500 cursor-pointer"
+            key={movie.id}
+          >
+            <img
+              src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+              alt=""
+              className="w-1/3 rounded-xl my-3"
+            />
+            <div className=" py-3">
+              <h3>{movie.title}</h3>
+              <span>{movie.release_date.split("-")[0]}</span>
+              <div className="flex">
+                {Array.from({ length: movie.vote_average }).map((_, index) => (
+                  <FaStar key={index} />
+                ))}
+              </div>
+              <span className="text-xs line-clamp-3 ">{movie.overview}</span>
+            </div>
+          </div>
         ))}
-      </div> */}
-      <div className="bg-gray-300 p-3">
+      </div>
+      {/* <div className="bg-gray-300 p-3">
         <div className="flex px-5 py-3 w-1/4 h-52 gap-1 border rounded-xl bg-white">
           <img
             src="https://image.tmdb.org/t/p/w200/vE2fYcP07fbIHoouATNErRQuNrw.jpg"
@@ -31,7 +53,7 @@ const MovieCard = () => {
             </span>
           </div>
         </div>
-      </div>
+      </div> */}
     </>
   );
 };
