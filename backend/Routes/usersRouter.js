080426ee0ca012fb1b9b75f2dabfec3b05b9acde
Mohammad Anas <mdanascal@gmail.com>
2026-07-17 20:21:25 +0530

    add input validation for signup endpoint

--- backend/Routes/usersRouter.js
+++ backend/Routes/usersRouter.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { registerUser, loginUser } from "../Controllers/usersController.js";
-import { loginValidator } from "../validators/AuthValidators.js";
+import {
+  loginValidator,
+  registerValidator,
+} from "../validators/AuthValidators.js";
 import validate from "../Middlewares/validationMiddleware.js";
 
 const router = express.Router();
 
-router.post(`/signup`, registerUser);
+router.post(`/signup`, registerValidator, validate, registerUser);
 router.post(`/login`, loginValidator, validate, loginUser);
 
 export default router;
--- backend/validators/AuthValidators.js
+++ backend/validators/AuthValidators.js
@@ -2,7 +2,19 @@ import { body } from "express-validator";
 
 const loginValidator = [
   body("password").notEmpty().withMessage("password required"),
-  body("email").isEmail().normalizeEmail(),
+  body("email")
+    .isEmail()
+    .normalizeEmail()
+    .withMessage("Incorrect Email format"),
 ];
 
-export { loginValidator };
+const registerValidator = [
+  body("password")
+    .trim()
+    .isLength({ max: 128, min: 8 })
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/),
+  body("email").isEmail().normalizeEmail(),
+  body("name").trim().notEmpty().isLength({ max: 50 }),
+];
+
+export { loginValidator, registerValidator };
