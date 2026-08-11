887fa9668e50f71ac0e65995d2b9dd3a240201e3
Mohammad Anas <mdanascal@gmail.com>
2026-08-11 19:56:13 +0530

    add fixed size for loading skeleton

--- frontend/src/components/ui/UserBtnSection.jsx
+++ frontend/src/components/ui/UserBtnSection.jsx
@@ -5,9 +5,9 @@ import { FaExclamationCircle } from "react-icons/fa";
 const UserBtnSection = ({ className = "", isLoading, isError, children }) => {
   if (isLoading) {
     return (
-      <div className={className}>
+      <div className="flex gap-2">
         {[...Array(3)].map((_, i) => (
-          <Skeleton key={i} circle width="100%" height="100%" />
+          <Skeleton key={i} circle width={30} height={30} />
         ))}
       </div>
     );
