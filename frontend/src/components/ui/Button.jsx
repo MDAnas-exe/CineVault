1ebcd18af900e59a3d0bd23c3993ffa94c1f1f4a
Mohammad Anas <mdanascal@gmail.com>
2026-08-13 10:44:02 +0530

    refactor prop consumption style

--- frontend/src/components/ui/Button.jsx
+++ frontend/src/components/ui/Button.jsx
@@ -1,17 +1,9 @@
 import { twMerge } from "tailwind-merge";
 
-const Button = ({
-  children,
-  type,
-  className = "",
-  isPending = false,
-  onClick,
-}) => {
+const Button = ({ children, className = "", ...props }) => {
   return (
     <button
-      onClick={onClick}
-      disabled={isPending}
-      type={type}
+      {...props}
       className={twMerge(
         "h-12 w-full cursor-pointer rounded-xl bg-accent font-poppins font-semibold text-primary shadow-sm transition-all duration-200 hover:bg-[#c89412] focus:outline-none focus:ring-2 focus:ring-accent/40 focus:ring-offset-2 active:scale-[0.99]",
         className,
