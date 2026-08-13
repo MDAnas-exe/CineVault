a534d9edd8559eceb7feba374025f39c9732657d
Mohammad Anas <mdanascal@gmail.com>
2026-08-13 21:14:59 +0530

    added reposiveness

--- frontend/src/components/ui/UserActionButton.jsx
+++ frontend/src/components/ui/UserActionButton.jsx
@@ -25,13 +25,13 @@ const CrossFadeIcon = ({ iconKey, isActive, label }) => {
   if (!icons) return null;
   const { outline: Outline, solid: Solid } = icons;
   return (
-    <span className={`relative size-4 ${!label && "size-full"} `}>
+    <span className={`relative size-3 lg:size-4 ${!label && "size-full"} `}>
       <Outline
-        className={`absolute inset-0 transition-opacity duration-200 lg:m-0 m-auto ${!label && "m-auto"}`}
+        className={`absolute inset-0 transition-opacity duration-200  ${!label && "m-auto"}`}
         style={{ opacity: isActive ? 0 : 1 }}
       />
       <Solid
-        className={`absolute inset-0 transition-opacity duration-200 lg:m-0 m-auto ${!label && "m-auto"}`}
+        className={`absolute inset-0 transition-opacity duration-200  ${!label && "m-auto"}`}
         style={{ opacity: isActive ? 1 : 0, color: "#D4A017" }}
       />
     </span>
