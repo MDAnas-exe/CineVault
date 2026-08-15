ab49aa40425bbd2feb9ceb76fccbbccfe0466c69
Mohammad Anas <mdanascal@gmail.com>
2026-08-15 10:45:57 +0530

    change schema

--- backend/Models/reviewModel.js
+++ backend/Models/reviewModel.js
@@ -2,14 +2,19 @@ import mongoose from "mongoose";
 
 const reviewSchema = new mongoose.Schema(
   {
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
     name: { type: String, required: true, trim: true },
-    movie: { type: Number, required: true },
+    movieId: { type: Number, required: true },
+    movieTitle: { type: String, required: true, trim: true },
     review: { type: String, required: true, trim: true },
   },
   { timestamps: true },
 );
 
-reviewSchema.index({ user: 1, movie: 1 }, { unique: true });
+reviewSchema.index({ userId: 1, movieId: 1 }, { unique: true });
 
 export default mongoose.model("Review", reviewSchema);
