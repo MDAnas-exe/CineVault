a78d59f5ed6b63a641c0e6a67230dd5695bedccf
Mohammad Anas <mdanascal@gmail.com>
2026-08-18 11:05:21 +0530

    add user review section for user reviews

--- frontend/src/features/movie/components/Reviews.jsx
+++ frontend/src/features/movie/components/Reviews.jsx
@@ -1,5 +1,17 @@
+import UserReviewSection from "./UserReviewSection";
+
 const Reviews = () => {
-  return <div>Reviews</div>;
+  return (
+    <section className="md:mt-8 mt-4">
+      <h2 className="mb-5 md:mb-10 font-poppins text-2xl md:text-5xl font-bold text-primary">
+        Reviews
+      </h2>
+
+      <UserReviewSection />
+
+      {/* TODO: MovieReviewsSection */}
+    </section>
+  );
 };
 
 export default Reviews;
