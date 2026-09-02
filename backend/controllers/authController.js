3106ccaf65a0fa4805b67d936232cd372fbb58a4
Mohammad Anas <mdanascal@gmail.com>
2026-09-03 01:02:52 +0530

    fix: update SameSite cookie attribute for production environment

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -101,7 +101,7 @@ const logoutController = expressAsyncHandler(async (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
   });
 
   res.sendStatus(204);
