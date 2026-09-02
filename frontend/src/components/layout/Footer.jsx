a83bb97b71bb5dbd71ceb1432223d20245e13d46
Mohammad Anas <mdanascal@gmail.com>
2026-09-02 20:33:12 +0530

    remove ghost links

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -11,16 +11,6 @@ const Footer = () => {
           <p>sharing your love for cinema.</p>
         </div>
 
-        <div className="font-inter text-base font-semibold text-primary sm:text-lg md:space-y-4 lg:text-xl">
-          <p className="cursor-pointer hover:text-accent transition-colors">
-            About
-          </p>
-
-          <p className="cursor-pointer hover:text-accent transition-colors">
-            Contact
-          </p>
-        </div>
-
         <div className="border-t border-gray-200 pt-2 md:pt-6">
           <p className="font-inter text-sm text-secondary sm:text-base lg:text-base">
             © 2026 CineVault
