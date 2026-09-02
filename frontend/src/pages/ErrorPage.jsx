8d2eec5d4b9dd1970b2febcd0f6df8e53947f089
Mohammad Anas <mdanascal@gmail.com>
2026-09-02 16:19:45 +0530

    add error page

---
+++ frontend/src/pages/ErrorPage.jsx
@@ -0,0 +1,17 @@
+import { useRouteError } from "react-router-dom";
+
+function ErrorPage() {
+  const error = useRouteError();
+
+  return (
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
+      <h1 className="text-4xl font-bold mb-2">Oops!</h1>
+      <h3 className="text-lg text-gray-400 mb-4">Something went wrong.</h3>
+      <p className="text-sm text-gray-500">
+        {error?.message || error?.statusText || "Unknown error"}
+      </p>
+    </div>
+  );
+}
+
+export default ErrorPage;
