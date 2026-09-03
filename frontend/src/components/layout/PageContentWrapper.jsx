3955193c5524a713e5a8b222db459c6201e12991
Mohammad Anas <mdanascal@gmail.com>
2026-09-03 11:13:45 +0530

    fix: add min-height to PageContentWrapper for improved layout consistency

--- frontend/src/components/layout/PageContentWrapper.jsx
+++ frontend/src/components/layout/PageContentWrapper.jsx
@@ -2,7 +2,12 @@ import { twMerge } from "tailwind-merge";
 
 const PageContentWrapper = ({ children, className = "" }) => {
   return (
-    <div className={twMerge("mx-auto w-full px-3 py-5 lg:w-85/100", className)}>
+    <div
+      className={twMerge(
+        "mx-auto w-full px-3 py-5 lg:w-85/100 min-h-[70vh]",
+        className,
+      )}
+    >
       {children}
     </div>
   );
