acd193fbd96d9479b00e49bf01b2bc15a0e62d14
Mohammad Anas <mdanascal@gmail.com>
2026-09-04 21:27:01 +0530

    feat: add @vercel/analytics dependency and integrate analytics component in main application

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode, lazy } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import { Analytics } from "@vercel/analytics/react";
 import AuthLayout from "./components/layout/AuthLayout.jsx";
 import RootLayout from "./components/layout/RootLayout.jsx";
 import UserLayout from "./components/layout/UserLayout.jsx";
@@ -81,6 +81,7 @@ createRoot(document.getElementById("root")).render(
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <Toaster />
+        <Analytics />
         <RouterProvider router={router} />
       </AuthProvider>
     </QueryClientProvider>
