4c5101236dbb464b5a4d145b2644ca0c406dd340
Mohammad Anas <mdanascal@gmail.com>
2026-09-05 23:35:41 +0530

    feat: add ThemeToggle component for theme switching functionality

---
+++ frontend/src/components/ui/ThemeToggle.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { FaSun, FaMoon } from "react-icons/fa";
+import useTheme from "../../hooks/useTheme";
+const ThemeToggle = () => {
+  const { isDark, setIsDark } = useTheme();
+
+  const toggleTheme = () => {
+    setIsDark((prev) => !prev);
+    document.documentElement.classList.toggle("dark");
+  };
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className={`relative size-10 flex items-center justify-center hover:bg-gray-400 duration-200 rounded-full cursor-pointer`}
+    >
+      <FaSun
+        className={`absolute inset-0 m-auto transition-opacity duration-300 text-yellow-500 ${
+          isDark ? "opacity-0" : "opacity-100"
+        }`}
+      />
+      <FaMoon
+        className={`absolute inset-0 m-auto transition-opacity duration-300 text-blue-400 ${
+          isDark ? "opacity-100" : "opacity-0"
+        }`}
+      />
+    </button>
+  );
+};
+
+export default ThemeToggle;
