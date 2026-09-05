48bb02ed6717cfbb7d6c62f4e739ce457a278230
Mohammad Anas <mdanascal@gmail.com>
2026-09-06 00:03:25 +0530

    feat: allow custom className prop for ThemeToggle component

--- frontend/src/components/ui/ThemeToggle.jsx
+++ frontend/src/components/ui/ThemeToggle.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import { FaSun, FaMoon } from "react-icons/fa";
 import useTheme from "../../hooks/useTheme";
-const ThemeToggle = () => {
+import { twMerge } from "tailwind-merge";
+const ThemeToggle = ({ className }) => {
   const { isDark, setIsDark } = useTheme();
 
   return (
     <button
       onClick={() => setIsDark((prev) => !prev)}
-      className={`relative size-10 flex items-center justify-center hover:bg-gray-400 duration-200 rounded-full cursor-pointer`}
+      className={twMerge(
+        `relative size-10 flex items-center justify-center hover:bg-gray-400 duration-200 rounded-full cursor-pointer`,
+        className,
+      )}
     >
       <FaSun
         className={`absolute inset-0 m-auto transition-opacity duration-300 text-yellow-500 ${
